b6d4ea6b06dcb02388a7055691bc42c642ecc81e
@sumit-n31! <srn3112@gmail.com>
2021-12-23 16:33:47 +0530

    Third Change

--- src/test/java/com/TestCases/TC_001.java
+++ src/test/java/com/TestCases/TC_001.java
@@ -7,11 +7,15 @@ import org.testng.annotations.Test;
 import util.CBT;
 import util.DataUtil;
 
-public class TC_001 extends CBT{
-	@Test(dataProvider="testdata",dataProviderClass=DataUtil.class)
+public class TC_001 extends CBT {
+	@Test(dataProvider = "testdata", dataProviderClass = DataUtil.class)
 	public void ValidateCustomer(Hashtable<String, String> inf) {
 		System.out.println(inf.get("ID"));
 
 	}
 
+	@Test
+	private void m2() {
+		System.out.println();
+	}
 }
